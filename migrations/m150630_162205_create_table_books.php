e08ce5258876603b487bfd33b5410d3e21dd3b66
Ziy Shem <dergesus@gmail.com>
2015-06-30 19:47:20 +0300

    fixed migrations

--- migrations/m150630_162205_create_table_books.php
+++ migrations/m150630_162205_create_table_books.php
@@ -8,15 +8,13 @@ class m150630_162205_create_table_books extends Migration
     public function up()
     {
 
-        $this->createTable(){
-            'id'=>Schema::TYPE_PK,
+        $this->createTable('books',['id'=>Schema::TYPE_PK,
             'name'=>Schema::TYPE_STRING,
             'date_create'=>Schema::TYPE_TIMESTAMP,
             'date_update'=>Schema::TYPE_TIMESTAMP,
-            'preview'=Schema::TYPE_STRING,
+            'preview'=>Schema::TYPE_STRING,
             'date'=>Schema::TYPE_DATE,
-            'author_id'=>Schema::TYPE_INTEGER
-        }
+            'author_id'=>Schema::TYPE_INTEGER]);
 
 
     }
