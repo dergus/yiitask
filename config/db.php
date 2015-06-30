28a83722636d906df737ec90ca1f9b2d40dddf34
Ziy Shem <dergesus@gmail.com>
2015-06-30 19:41:45 +0300

    initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiitask',
+    'username' => 'roradmin',
+    'password' => 'rorssap',
+    'charset' => 'utf8',
+];
---
+++ migrations/m150630_162205_create_table_books.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150630_162205_create_table_books extends Migration
+{
+    public function up()
+    {
+
+        $this->createTable(){
+            'id'=>Schema::TYPE_PK,
+            'name'=>Schema::TYPE_STRING,
+            'date_create'=>Schema::TYPE_TIMESTAMP,
+            'date_update'=>Schema::TYPE_TIMESTAMP,
+            'preview'=Schema::TYPE_STRING,
+            'date'=>Schema::TYPE_DATE,
+            'author_id'=>Schema::TYPE_INTEGER
+        }
+
+
+    }
+
+    public function down()
+    {
+        echo "m150630_162205_create_table_books cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
